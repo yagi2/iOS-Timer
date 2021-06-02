14dc493ed9326e74306fa7f3da114c4f92eeda6c
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-02 21:25:31 +0900

    make layouts

---
+++ Timer/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  Timer
+//
+//  Created by Itsuki Aoyagi on 2021/06/02.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var timerSettingPicker: UIPickerView!
+    
+    @IBAction func decisionButtonAction(_ sender: Any) {
+        
+    }
+}
--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var countDownLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func settingButtonAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func startButtonAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func stopButtonAction(_ sender: Any) {
+        
+    }
 }
-
