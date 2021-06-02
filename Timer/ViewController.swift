e054002e67e6198c4e03ac2482e44944c430f305
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-02 21:33:27 +0900

    implement timer and navigate setting screen

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,21 +10,71 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var countDownLabel: UILabel!
-
+    
+    private var timer: Timer?
+    private var count = 0
+    private let settingKey = "timer_value"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let settings = UserDefaults.standard
+        settings.register(defaults: [settingKey:10])
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        count = 0
+        _ = displayUpdate()
     }
     
     @IBAction func settingButtonAction(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                nowTimer.invalidate()
+            }
+        }
         
+        performSegue(withIdentifier: "goSetting", sender: nil)
     }
     
     @IBAction func startButtonAction(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                return
+            }
+        }
         
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(timerInterrput(_:)),
+                                     userInfo: nil,
+                                     repeats: true)
     }
     
     @IBAction func stopButtonAction(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                nowTimer.invalidate()
+            }
+        }
+    }
+    
+    private func displayUpdate() -> Int {
+        let settings = UserDefaults.standard
+        let timerValue = settings.integer(forKey: settingKey)
+        let remainCount = timerValue - count
         
+        countDownLabel.text = "残り\(remainCount)秒"
+        
+        return remainCount
+    }
+    
+    @objc
+    private func timerInterrput(_ timer: Timer) {
+        count += 1
+        if displayUpdate() <= 0 {
+            count = 0
+            timer.invalidate()
+        }
     }
 }
